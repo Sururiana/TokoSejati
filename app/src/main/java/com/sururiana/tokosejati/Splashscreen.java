4a49c8538fc58309df92d781394ce96e0d5fc142
Zainul <zainul.arif97.za@gmail.com>
2020-05-05 15:33:42 +0700

    Splash

--- app/src/main/java/com/sururiana/tokosejati/Splashscreen.java
+++ app/src/main/java/com/sururiana/tokosejati/Splashscreen.java
@@ -2,13 +2,25 @@ package com.sururiana.tokosejati;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splashscreen extends AppCompatActivity {
+    private final int LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                Splashscreen.this.startActivity(intent);
+                Splashscreen.this.finish();
+            }
+        },LENGTH);
     }
 }
